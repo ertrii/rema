67988ba3be3ede04c61912fb48fe4092d9b4ba85
ertrii <ertriilive@gmail.com>
2025-02-11 16:27:30 -0500

    fallback for useCurrentState

--- src/lib/useCurrentState.ts
+++ src/lib/useCurrentState.ts
@@ -4,9 +4,10 @@ import { RemaKeyName, RemaProviderContext } from "./contexts";
 /**
  * Returns the current state of the keyName
  * @param keyName
+ * @param fallback
  */
-export default function useCurrentState<T>(keyName: RemaKeyName) {
+export default function useCurrentState<T>(keyName: RemaKeyName, fallback?: T) {
   const context = useContext(RemaProviderContext);
   const values = context.getState(keyName) as T;
-  return values;
+  return values || fallback;
 }
